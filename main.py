9b792a54b122fc90570793bfa56c04ec98b65dac
Robin Gerster <31821147+RobinGerster@users.noreply.github.com>
2023-04-11 14:28:27 +0200

    Basic Setup

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+from neo4j import GraphDatabase
+from graphdatascience import GraphDataScience
+
+# Connect to Neo4j
+HOST = 'neo4j://localhost'
+USERNAME = 'neo4j'
+PASSWORD = 'password'
+uri = "bolt://localhost:7687" # Replace with your Neo4j Desktop bolt address (should be the same)
+username = "neo4j" # Replace with your Neo4j Desktop username (should be the same)
+password = "password" # Replace with your Neo4j Desktop password (should be the same)
+
+# Setup necessary driver and gds for running cypher in our DBMS
+driver = GraphDatabase.driver(uri, auth=(username, password))
+gds = GraphDataScience(driver)
+
+#Example script to return a table of node counts.
+# total node counts
+result = gds.run_cypher('''
+    CALL apoc.meta.stats()
+    YIELD labels
+    UNWIND keys(labels) AS nodeLabel
+    RETURN nodeLabel, labels[nodeLabel] AS nodeCount
+''')
+print(result)
